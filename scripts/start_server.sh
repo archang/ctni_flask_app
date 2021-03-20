2f67cc9452c94c318151c96c8e5d422f186fe5de
Yashoza19 <yoyashoza@gmail.com>
2021-03-20 00:27:41 -0400

    Changing Start_server.sh:

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 cd /home/ubuntu/server
 source environment/bin/activate
-nohup gunicorn --bind 0.0.0.0:8000 wsgi > /home/ubuntu/output.txt 2> /home/ubuntu/erro.txt &
+nohup gunicorn --bind 0.0.0.0:8000 > /home/ubuntu/output.txt 2> /home/ubuntu/erro.txt &
